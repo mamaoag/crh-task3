4e5ccf50802410936a37a6e23ed58df58f75fb9b
Jio Mamaoag <jiomamaoag@gmail.com>
2020-06-19 16:28:36 +0800

    Initial Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+@extends('template.master')
+
+@section('content')
+<form action="{{route('login')}}" method="POST">
+  <div class="form-group">
+    <label for="exampleInputEmail1">Username</label>
+    <input type="text" name="uid" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" name="pwd" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+@endsection
